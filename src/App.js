d28876cdbd8e8004cb2735d5e841cc0948ad3d9b
Your Name <patrick@Patricks-MacBook-Air.local>
2023-08-16 14:42:51 +0300

    fixed add user grade on the dialog

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Suspense } from 'react';
+ import React, { Suspense } from 'react';
 import { createGlobalStyle } from "styled-components";
 
 import 'react-quill/dist/quill.snow.css';
